98625d292a9a9ae834b2cf54384a1b27b8a4e7b2
Luan Victor Souza Rocha <luan.rocha@dcoimbra.com.br>
2023-03-04 12:49:30 +0000

    fix: Resolvendo problema de duplicata no schema Bearer

--- src/ApiStorageManager.WebApi/Program.cs
+++ src/ApiStorageManager.WebApi/Program.cs
@@ -41,25 +41,11 @@ builder.Services.AddSwaggerGen(c =>
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         In = ParameterLocation.Header,
-        Description = "Insira um token v·lido",
+        Description = "Insira um token v√°lido",
         Name = "Authorization",
         Type = SecuritySchemeType.Http,
         BearerFormat = "JWT",
         Scheme = "Bearer"
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[]{}
-        }
     });
 });
 builder.Services.AddAutoMapperConfiguration();
